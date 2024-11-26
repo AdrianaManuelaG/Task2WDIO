762a4003888bfac54b0dfb886968887f69ee0fe6
Adriana Gula <adrianagula25@gmail.com>
2024-11-26 13:30:19 +0200

    update practice3

--- test/specs/editUserProfile.spec.js
+++ test/specs/editUserProfile.spec.js
@@ -1,7 +1,7 @@
 describe("editUserProfile", () => {
     it("User successfully Login on Trello", async() => {
         await browser.url("https://trello.com/");
-        await $(".Buttonsstyles__Button-sc-1jwidxo-0").click();
+        await $("[data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").click();
         await $("#username").setValue("adrianagula25@gmail.com");
         await $(".css-t8mx7z").click();
         await $("#password").setValue("Adrianagula2024!");
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,7 +1,9 @@
+
+
 describe("Log in on Trello", () => {
-    it("User successfully Login on Trello", async() => {
+    it("User should successfully Login on Trello", async() => {
         await browser.url("https://trello.com/");
-        await $('.Buttonsstyles__Button-sc-1jwidxo-0').click();
+        await $("[data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").click();
         await $("#username").setValue("adrianagula25@gmail.com");
         await $(".css-t8mx7z").click();
         await $("#password").setValue("Adrianagula2024!");
@@ -14,9 +16,6 @@ describe("Log in on Trello", () => {
         //     console.log("New window is displayed,successfull test");
         //     return; 
         // }
-        await browser.waitUntil(() => browser.getUrl().then(url => url.includes("/boards")), {
-            timeout: 1000,
-        });
     });
 
 });
